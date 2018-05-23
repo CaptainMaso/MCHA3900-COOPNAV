da467366b3fbe99a365146932e4e40db0c7385e0
c3218752 <c3218752@CES-409002.uncle.newcastle.edu.au>
2018-05-23 15:54:59 +1000

    -changed R notation

--- Sensors/GetHAPData.m
+++ Sensors/GetHAPData.m
@@ -1,3 +1,3 @@
 function y_HAP = GetHAPData(X_AUV, X_WAMV)
-J = eulerRotation(XWAMV(4:6));
-y_HAP = J*(X_AUV(1:3)-X_WAMV(1:3));
+Rwn = eulerRotation(XWAMV(4:6));
+y_HAP = Rwn*(X_AUV(1:3)-X_WAMV(1:3));
